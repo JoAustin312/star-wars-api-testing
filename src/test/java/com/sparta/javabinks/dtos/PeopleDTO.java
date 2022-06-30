1367db049f1ecfeaf99f8a3f0cf43bb5da831a46
WillTran02 <williamqtran@hotmail.co.uk>
2022-06-30 12:04:08 +0100

    Added People DTO, added more overloaded ConnectionManager.getConnection() method

---
+++ src/test/java/com/sparta/javabinks/dtos/PeopleDTO.java
@@ -0,0 +1,146 @@
+package com.sparta.javabinks.dtos;
+
+import java.util.Arrays;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PeopleDTO{
+
+	@JsonProperty("films")
+	private List<String> films;
+
+	@JsonProperty("homeworld")
+	private String homeworld;
+
+	@JsonProperty("gender")
+	private String gender;
+
+	@JsonProperty("skin_color")
+	private String skinColor;
+
+	@JsonProperty("edited")
+	private String edited;
+
+	@JsonProperty("created")
+	private String created;
+
+	@JsonProperty("mass")
+	private String mass;
+
+	@JsonProperty("vehicles")
+	private List<String> vehicles;
+
+	@JsonProperty("url")
+	private String url;
+
+	@JsonProperty("hair_color")
+	private String hairColor;
+
+	@JsonProperty("birth_year")
+	private String birthYear;
+
+	@JsonProperty("eye_color")
+	private String eyeColor;
+
+	@JsonProperty("species")
+	private List<String> species;
+
+	@JsonProperty("starships")
+	private List<String> starships;
+
+	@JsonProperty("name")
+	private String name;
+
+	@JsonProperty("height")
+	private String height;
+
+	public List<String> getFilms(){
+		return films;
+	}
+
+	public String getHomeworld(){
+		return homeworld;
+	}
+
+	public String getGender(){
+		return gender;
+	}
+
+	public String getSkinColor(){
+		return skinColor;
+	}
+
+	public String getEdited(){
+		return edited;
+	}
+
+	public String getCreated(){
+		return created;
+	}
+
+	public String getMass(){
+		return mass;
+	}
+
+	public List<String> getVehicles(){
+		return vehicles;
+	}
+
+	public String getUrl(){
+		return url;
+	}
+
+	public String getHairColor(){
+		return hairColor;
+	}
+
+	public String getBirthYear(){
+		return birthYear;
+	}
+
+	public String getEyeColor(){
+		return eyeColor;
+	}
+
+	public List<String> getSpecies(){
+		return species;
+	}
+
+	public List<String> getStarships(){
+		return starships;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getHeight(){
+		return height;
+	}
+
+	public List<String> getPossibleGenders() {
+		return Arrays.asList(
+				"male",
+				"female",
+				"unknown",
+				"n/a"
+		);
+	}
+
+	public boolean hasBeenInAFilm() { return films != null && !films.isEmpty(); }
+	public boolean hasHomeworld() { return homeworld != null; }
+	public boolean hasGender() { return gender != null; }
+	public boolean hasSkinColor() { return skinColor != null; }
+	public boolean hasEditedDate() { return edited != null; }
+	public boolean hasCreatedDate() { return created != null; }
+	public boolean hasMass() { return mass != null; }
+	public boolean hasVehicles() { return vehicles != null && !vehicles.isEmpty(); }
+	public boolean hasUrl() { return url != null; }
+	public boolean hasHairColor() { return hairColor != null; }
+	public boolean hasBirthYear() { return birthYear != null; }
+	public boolean hasEyeColor() { return eyeColor != null; }
+	public boolean hasSpecies() { return species != null && !species.isEmpty(); }
+	public boolean hasStartships() { return starships != null && !starships.isEmpty(); }
+	public boolean hasName() { return name != null; }
+	public boolean hasHeight() { return height != null; }
+}
--- src/test/java/com/sparta/javabinks/framework/ConnectionManager.java
+++ src/test/java/com/sparta/javabinks/framework/ConnectionManager.java
@@ -10,21 +10,37 @@ public class ConnectionManager {
 
     private static final String BASEURL = "https://swapi.dev/api/";
 
-    public static String getConnection() {
+    public static String getConnection() { //retrieve root url
         getResponse();
         return BASEURL;
     }
-    public static String getConnection(String option, String value) {
+    public static String getConnection(String option, String value) { //search query
         getResponse();
         return BASEURL + "?" + option + "=" + value;
     }
-    public static String getConnection(String option, int value) {
+    public static String getConnection(String option, int value) { //search query
         getResponse();
         return BASEURL + "?" + option + "=" + value;
     }
-    public static String getConnection(String option, double value) {
+    public static String getConnection(String option, double value) { //search query
         getResponse();
         return BASEURL + "?" + option + "=" + value;
+    }
+    public static String getConnection(String extension) { //retrieve deeper resource
+        getResponse();
+        return BASEURL + extension;
+    }
+    public static String getConnection(String extension, String option, String value) { //retrieve deeper resource + query
+        getResponse();
+        return BASEURL + extension + "?" + option + "=" + value;
+    }
+    public static String getConnection(String extension, String option, int value) { //retrieve deeper resource + query
+        getResponse();
+        return BASEURL + extension + "?" + option + "=" + value;
+    }
+    public static String getConnection(String extension, String option, double value) { //retrieve deeper resource + query
+        getResponse();
+        return BASEURL + extension + "?" + option + "=" + value;
     }
     public static int getStatusCode() { return getResponse().statusCode(); }
     private static HttpResponse<String> getResponse() {

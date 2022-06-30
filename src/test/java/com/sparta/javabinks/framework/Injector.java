0cde50c92583af2c40254715ee065eccac27665c
J Hibbert <jhibbert@spartaglobal.com>
2022-06-30 14:35:57 +0100

    createde injector methods for each DTO

--- src/test/java/com/sparta/javabinks/framework/Injector.java
+++ src/test/java/com/sparta/javabinks/framework/Injector.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.net.URL;
 
 public class Injector {
+
     public static PeopleDTO injectPeopleDTO(String url) {
         PeopleDTO peopleDTO = null;
         ObjectMapper mapper = new ObjectMapper();
@@ -16,5 +17,56 @@ public class Injector {
             e.printStackTrace();
         }
         return peopleDTO;
+    }
+    public static FilmDTO injectFilmDTO(String url) {
+        FilmDTO filmDTO = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            filmDTO = mapper.readValue(new URL(url), filmDTO.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return filmDTO;
+    }
+    public static PlanetsDTO injectPlantsDTO(String url) {
+        PlanetsDTO planetsDTO = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            planetsDTO = mapper.readValue(new URL(url), planetsDTO.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return planetsDTO;
+    }
+    public static StarshipsDTO injectStarshipsDTO(String url) {
+        StarshipsDTO starshipsDTO = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            starshipsDTO = mapper.readValue(new URL(url), starshipsDTO.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return starshipsDTO;
+    }
+    public static SpeciesDTO injectSpeciesDTO(String url) {
+        SpeciesDTO speciesDTO = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            speciesDTO = mapper.readValue(new URL(url), speciesDTO.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return speciesDTO;
+    }
+    
+    public static VehiclesDTO injectVehiclesDTO(String url) {
+        VehiclesDTO vehiclesDTO = null;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            vehiclesDTO = mapper.readValue(new URL(url), vehiclesDTO.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return vehiclesDTO;
     }
 }
